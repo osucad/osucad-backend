75dd61cd262d9bd88d9153f9eb54b6267ab8bc24
Marvin <minetoblend@gmail.com>
2024-11-21 22:26:57 +0000

    Install websocket plugin

--- app/websocket-gateway/src/main/kotlin/com/osucad/server/websocketGateway/Application.kt
+++ app/websocket-gateway/src/main/kotlin/com/osucad/server/websocketGateway/Application.kt
@@ -1,20 +1,24 @@
 package com.osucad.server.websocketGateway
 
 import com.osucad.server.websocketGateway.plugins.configureRedis
+import com.osucad.server.websocketGateway.plugins.configureWebSockets
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+    configureWebSockets()
     configureRedis()
 
     routing {
-        get {
-            call.respond("Hello, world!")
+        webSocket("/ws") {
+            send("Hello, world!")
         }
     }
 }
---
+++ app/websocket-gateway/src/main/kotlin/com/osucad/server/websocketGateway/plugins/WebSockets.kt
@@ -0,0 +1,24 @@
+package com.osucad.server.websocketGateway.plugins
+
+import io.ktor.serialization.kotlinx.*
+import io.ktor.server.application.*
+import io.ktor.server.websocket.*
+import kotlinx.serialization.json.ClassDiscriminatorMode
+import kotlinx.serialization.json.Json
+import kotlin.time.Duration.Companion.seconds
+
+fun Application.configureWebSockets() {
+    val isDevelopmentMode = developmentMode
+
+    install(WebSockets) {
+        pingPeriod = 15.seconds
+        timeout = 15.seconds
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+        contentConverter = KotlinxWebsocketSerializationConverter(Json {
+            prettyPrint = isDevelopmentMode
+            ignoreUnknownKeys = true
+            encodeDefaults = true
+        })
+    }
+}
