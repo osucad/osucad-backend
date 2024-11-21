44bc1469f5b6770e598f860084c535bac0396c07
Marvin <minetoblend@gmail.com>
2024-11-21 21:48:05 +0000

    Setup ktor

--- app/websocket-gateway/build.gradle.kts
+++ app/websocket-gateway/build.gradle.kts
@@ -4,7 +4,15 @@ plugins {
     alias(libs.plugins.ktor)
 }
 
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
 dependencies {
     implementation(libs.bundles.kotlinxEcosystem)
     implementation(libs.bundles.lettuce)
+    implementation(libs.bundles.ktor)
 }
--- app/websocket-gateway/src/main/kotlin/com/osucad/server/websocketGateway/Application.kt
+++ app/websocket-gateway/src/main/kotlin/com/osucad/server/websocketGateway/Application.kt
@@ -1,5 +1,17 @@
 package com.osucad.server.websocketGateway
 
-fun main() {
-    println("Hello World!")
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    routing {
+        get {
+            call.respond("Hello, world!")
+        }
+    }
 }
