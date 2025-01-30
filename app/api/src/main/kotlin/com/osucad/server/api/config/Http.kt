361a4c612fdd936b30756749af11515e6e7c7342
Marvin Schürz <m.schuerz@hautzy.com>
2025-01-30 10:44:14 +0100

    Add CORS configuration in development mode

--- app/api/src/main/kotlin/com/osucad/server/api/config/Http.kt
+++ app/api/src/main/kotlin/com/osucad/server/api/config/Http.kt
@@ -3,6 +3,7 @@ package com.osucad.server.api.config
 import com.osucad.server.api.exceptions.ResponseStatusException
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.CORS
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 
@@ -15,6 +16,14 @@ fun Application.configureErrorHandling() {
         exception<Exception> { call, cause ->
             call.respond(HttpStatusCode.InternalServerError, "Internal server error")
             throw cause
+        }
+    }
+
+    if (developmentMode) {
+        install(CORS) {
+            anyHost()
+            anyMethod()
+            allowCredentials = true
         }
     }
 }
