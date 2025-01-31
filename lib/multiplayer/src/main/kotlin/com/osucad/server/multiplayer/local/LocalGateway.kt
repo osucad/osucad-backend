43c20c5af064134e1fe8f01ea159ef2f91c68d49
Marvin Schürz <m.schuerz@hautzy.com>
2025-01-31 01:22:55 +0100

    Add warning when using local gateway

--- lib/multiplayer/src/main/kotlin/com/osucad/server/multiplayer/local/LocalGateway.kt
+++ lib/multiplayer/src/main/kotlin/com/osucad/server/multiplayer/local/LocalGateway.kt
@@ -4,10 +4,17 @@ import com.osucad.server.multiplayer.IWebSocket
 import com.osucad.server.multiplayer.IWebsocketGateway
 import com.osucad.server.multiplayer.types.UserInfo
 import io.micrometer.core.instrument.MeterRegistry
+import org.slf4j.LoggerFactory
 import java.util.Collections
 import java.util.UUID
 
 class LocalGateway(private val meterRegistry: MeterRegistry) : IWebsocketGateway {
+    private val logger = LoggerFactory.getLogger(LocalGateway::class.java)
+
+    init {
+        logger.warn("LocalGateway is intended for development only, please use RedisGateway in production")
+    }
+
     private val clientManagers = Collections.synchronizedMap(mutableMapOf<UUID, LocalClientManager>())
     private val opsManagers = Collections.synchronizedMap(mutableMapOf<UUID, LocalOpsManager>())
     private val broadcaster = LocalMessageBroadcaster()
